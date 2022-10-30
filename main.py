90a1089400ae53f840bcc707596d811408785952
dimis1995 <dimitrisdelis1995@gmal.com>
2022-10-30 20:43:30 +0200

    Refactored code to be a bit more readable

--- main.py
+++ main.py
@@ -4,59 +4,28 @@ import pandas as pd
 from prophet import Prophet
 import matplotlib.pyplot as plt
 
+
+def model_and_predict(dataset: pd.DataFrame, title: str, columns: [str]):
+    for column in columns:
+        changed_names = {'Date': 'ds', column: 'y'}
+        dataset_copy = dataset.rename(columns=changed_names)
+
+        model = Prophet()
+        model.fit(dataset_copy)
+        future = model.make_future_dataframe(periods=365)
+        forecast = model.predict(future)
+
+        fig = model.plot(forecast, ylabel=column, xlabel='Time', include_legend=True)
+        ax = fig.gca()
+        ax.set_title(title, size=6)
+
+
 if __name__ == '__main__':
-    # url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey='
-    # url += config.MY_API
-    # response = requests.get(url)
 
-    columns = ['open', 'high', 'low', 'close', 'volume']
+    picked_columns = ['open', 'high', 'low', 'close', 'volume']
 
-    aapl = pd.read_csv('AAPL.csv')
-    for column in columns:
-        if column == 'Date':
-            continue
-        dict = {'Date':'ds', column: 'y'}
-        aapl_copy = aapl.rename(columns=dict)
-
-        model = Prophet()
-        model.fit(aapl_copy)
-        future = model.make_future_dataframe(periods=365)
-        forecast = model.predict(future)
-
-        fig = model.plot(forecast, ylabel=column, xlabel='Time', include_legend=True)
-        ax = fig.gca()
-        ax.set_title('Apple', size=6)
-
-    msft = pd.read_csv('MSFT.csv')
-    for column in columns:
-        if column == 'Date':
-            continue
-        dict = {'Date': 'ds', column: 'y'}
-        msft_copy = msft.rename(columns=dict)
-
-        model = Prophet()
-        model.fit(msft_copy)
-        future = model.make_future_dataframe(periods=365)
-        forecast = model.predict(future)
-
-        fig = model.plot(forecast, ylabel=column, xlabel='Time', include_legend=True)
-        ax = fig.gca()
-        ax.set_title('Microsoft', size=6)
-
-    ibm = pd.read_csv('IBM.csv')
-    for column in columns:
-        if column == 'Date':
-            continue
-        dict = {'Date': 'ds', column: 'y'}
-        ibm_copy = ibm.rename(columns=dict)
-
-        model = Prophet()
-        model.fit(ibm_copy)
-        future = model.make_future_dataframe(periods=365)
-        forecast = model.predict(future)
-
-        fig = model.plot(forecast, ylabel=column, xlabel='Time', include_legend=True)
-        ax = fig.gca()
-        ax.set_title('Apple', size=6)
+    model_and_predict(pd.read_csv('AAPL.csv'), 'Apple', picked_columns)
+    model_and_predict(pd.read_csv('MSFT.csv'), 'Microsoft', picked_columns)
+    model_and_predict(pd.read_csv('IBM.csv'), 'IBM', picked_columns)
 
     plt.show()
